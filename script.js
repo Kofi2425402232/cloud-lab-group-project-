e3727e378390c94a0d473e0e17204a2d6bdea170
Kofi2425402232 <2425402232@live.gctu.edu.gh>
2026-06-17 23:11:23 +0000

    Add files via upload
    Hosted branch

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("historyBtn")?.addEventListener("click", () => {
+    document.getElementById("message").textContent =
+        "Bash was created in 1989 by Brian Fox for the GNU Project and remains one of the most widely used Unix shells today.";
+});
+
